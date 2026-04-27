26e0ea12ca6dfdc34a04f6ee37c4f183a807e170
Anurag-Solanki1 <solankimayank028.com>
2026-04-27 18:58:37 +0530

    Remove test

--- TestJwt.java
+++
@@ -1,9 +0,0 @@
-import com.codechecker.security.JwtService;
-import com.codechecker.entity.UserEntity;
-import org.springframework.test.util.ReflectionTestUtils;
-
-public class TestJwt {
-    public static void main(String[] args) throws Exception {
-        // Can't easily spin up JwtService outside spring boot. Let's just grab the secret from application.properties
-    }
-}
